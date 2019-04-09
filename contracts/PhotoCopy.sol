07adea1edd679ee7711ae2bcbfb524a4068cd982
Bryant <cabrera.bryant@gmail.com>
2019-04-08 18:43:16 -0700

    Bryant updates .env file.

--- contracts/PhotoCopy.sol
+++ contracts/PhotoCopy.sol
@@ -9,4 +9,16 @@ contract PhotoCopy {
     //public lets us read the value of taskCount
     //initialize to 0
     uint public taskCount = 0;
+
+    struct Task {
+        //uint is a datatype that is an unassigned integer that can't be negative
+        uint id;
+        string content;
+        bool completed; //state of item
+    }
+
+    //put these tasks in storage on blockchain
+    //create new var with mapping datatype
+        //mapping datatype ~hash where you store a key value pair (datatype => TheTaskNameFromStruct)
+    mapping(uint => Task) tasks;
 }
